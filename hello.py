ffa278beff53a5c5885f551dda0f11bee383d7ad
Ameek Singh <ameek@Ameeks-MacBook-Air.local>
2023-09-21 14:44:14 -0400

    Lab 1 - Activity 3: Complete.

--- hello.py
+++ hello.py
@@ -9,7 +9,7 @@ moment = Moment(app)
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template('index.html', time_stamp=datetime.utcnow())
  
 @app.route("/user/<name>")
 def user(name):
