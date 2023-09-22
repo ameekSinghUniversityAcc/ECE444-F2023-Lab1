6bcfab90ad2fb6b4b0e351485b4dfb4b6ee48e71
Ameek Singh <ameeks.singh@mail.utoronto.ca>
2023-09-21 21:17:52 -0400

    Lab 1 - Activity 4: Code complete: Added in email field with validation. The email field validates for the presence of @ and utoronto in the email.

--- hello.py
+++ hello.py
@@ -5,10 +5,14 @@ from datetime import datetime
 
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email
+
+def UofTEmailValidator(email):
+    return True if "utoronto" in email else False
 
 class NameForm(FlaskForm):
     name = StringField("What is your name?", validators=[DataRequired()])
+    email = StringField("What is your UofT email address?", validators=[DataRequired(), Email()])
     submit = SubmitField('Submit')
 
 app = Flask(__name__)
@@ -25,10 +29,16 @@ def index():
         if old_name is not None and old_name != form.name.data:
             flash("Looks like you have changed your name!")
 
-        session['name'] = form.name.data
-        return redirect(url_for('index'))
+        # If valid UofT email entered, clear out fields and preserve the value
+        if UofTEmailValidator(form.email.data):
+            session['name'] = form.name.data
+            session['email'] = form.email.data
+            return redirect(url_for('index'))
 
-    return render_template('index.html', form=form, name=session.get('name'), time_stamp=datetime.utcnow())
+        # Else, flash a warning
+        else:
+            flash("Please use a valid UofT email")
+    return render_template('index.html', form=form, name=session.get('name'), email=session.get('email'), time_stamp=datetime.utcnow())
  
 @app.route("/user/<name>")
 def user(name):
